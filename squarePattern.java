8c6347d37928685f12fffb928b31bc8d430be8e7
mayurivirkar <128687719+mayurivirkar@users.noreply.github.com>
2025-06-26 15:53:01 +0530

    Add files via upload

---
+++ squarePattern.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class squarePattern {
+
+    public static void main(String[] args) {
+
+     for (int i = 0; i <= 4; i++){
+
+         for (int j = 0; j <= 5; j++){
+             System.out.print("*");
+         }
+
+         System.out.println("*");
+     }
+
+    }
+}
